4049917ca8d92ca9bee457394380c96d3e7b5620
azeelleeza <leezaluncheon@gmail.com>
2018-06-07 11:46:09 -0400

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,19 @@
 export default (state = [], action) => {
-  return state;
-}
+  let quote;
+  switch (action.type) {
+    case "ADD_QUOTE":
+      return [...state, action.quote];
+    case "REMOVE_QUOTE":
+      return state.filter(quote => quote.id !== action.quoteId);
+    case "UPVOTE_QUOTE":
+      quote = state.find(quote => action.quoteId);
+      ++quote.votes;
+      return state;
+    case "DOWNVOTE_QUOTE":
+      quote = state.find(quote => action.quoteId);
+      if (quote.votes > 0) --quote.votes;
+      return state;
+    default:
+      return state;
+  }
+};
